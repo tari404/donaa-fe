ef82d0df46cbf1f5e109407b561e3b4808e6ae98
tari404 <2680383605@qq.com>
2019-07-20 17:26:18 +0800

    ⚙️ update

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -14,6 +14,31 @@
               <p class="main">{{q.intro}}</p>
             </el-main>
           </el-container>
+          <el-main style="padding: 0;">
+            <hr style="border-bottom: none; opacity: .4;">
+            <div class="number-infos">
+              <div>
+                <span>{{q.starttime}}</span>
+                <span>Start</span>
+              </div>
+              <div>
+                <span>{{q.endtime}}</span>
+                <span>End</span>
+              </div>
+              <div>
+                <span>{{q.count}}</span>
+                <span>Donations</span>
+              </div>
+              <div>
+                <span>{{(q.amount / 1e18).toFixed(2)}}</span>
+                <span>Total</span>
+              </div>
+              <div>
+                <span>{{(q.expectedAmount / 1e18).toFixed(2)}}</span>
+                <span>Expected Amount</span>
+              </div>
+            </div>
+          </el-main>
           <el-footer style="padding: 0">
             <br>
             <el-input
@@ -23,49 +48,41 @@
               v-model="money[q.id]">
             </el-input>
             <span>&nbsp;&nbsp;</span>
-            <el-button plain size="mini" @click="donate(q.id)">DONATE</el-button>
+            <el-button plain size="mini" @click="openModal(q.id)">DONATE</el-button>
             <el-button plain size="mini" @click="toDetail(q.id)" class="button details">DETAILS</el-button>
           </el-footer>
         </el-container>
         </el-card>
       </el-col>
     </el-row>
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      append-to-body="true"
+      width="30vw">
+      <span>你可以选择留下仅基金会可见的联系邮箱</span>
+      <br>
+      <el-input
+        class="donate-input"
+        size="mini"
+        placeholder="E-mail"
+        v-model="email">
+      </el-input>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="donate">匿名</el-button>
+        <el-button type="primary" @click="donate">确认</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { mapMutations, mapActions } from 'vuex'
-
-// const testData = [
-//   {
-//     id: "0x1f9a7f61892ea23128db0469f77262fd5a33a38b",
-//     name: "2018-19 L-Istrina Campaign",
-//     intro: "The Malta Community Chest Foundation seeks crypto donation to support its work that providing financial, material and professional support to people experiencing difficulties because of cancer or severe chronic diseases, ",
-//     img: "https://resource.binance.charity/images/0257601e7b664f1cbe0b62491ee4f7b7_4.jpg",
-//     expectedAmount: "500",
-//     amount: "300"
-//   },
-//   {
-//     id: "0x1f9a7f61892ea23128db0469f77262fd5a33a38c",
-//     name: "2018-19 L-Istrina Campaign",
-//     intro: "The Malta Community Chest Foundation seeks crypto donation to support its work that providing financial, material and professional support to people experiencing difficulties because of cancer or severe chronic diseases, ",
-//     img: "https://resource.binance.charity/images/0257601e7b664f1cbe0b62491ee4f7b7_4.jpg",
-//     expectedAmount: "500",
-//     amount: "300"
-//   },
-//   {
-//     id: "0x1f9a7f61892ea23128db0469f77262fd5a33a38d",
-//     name: "2018-19 L-Istrina Campaign",
-//     intro: "The Malta Community Chest Foundation seeks crypto donation to support its work that providing financial, material and professional support to people experiencing difficulties because of cancer or severe chronic diseases, ",
-//     img: "https://resource.binance.charity/images/0257601e7b664f1cbe0b62491ee4f7b7_4.jpg",
-//     expectedAmount: "500",
-//     amount: "300"
-//   }
-// ]
+import { mapMutations, mapActions, mapState } from 'vuex'
 
 export default {
   name: 'Info',
   computed: {
+    ...mapState(['utils']),
     compressProjects () {
       const compressed = []
       for (let i = 0; i < this.projects.length; i += 2) {
@@ -81,22 +98,29 @@ export default {
   data () {
     const money = []
     return {
+      interval: 0,
+      dialogVisible: false,
       projects: [],
-      money
+      money,
+      email: ''
     }
   },
   watch: {
     projects (val) {
-      this.money = []
-      val.forEach(v => {
-        this.money[v.id] = ''
-      })
+      while (val.length > this.money.length) {
+        this.money.push('')
+      }
     }
   },
   created () {
     this.getProjects().then(projects => {
       this.projects = projects
     })
+    this.interval = setInterval(async () => {
+      const projects = await this.getProjects()
+      while(this.projects.pop()) {}
+      projects.forEach(v => this.projects.push(v))
+    }, 2000)
   },
   methods: {
     ...mapMutations(['selectProject']),
@@ -109,8 +133,21 @@ export default {
       this.donation({ id, value, hash: '0x7fb210d89587ecf000127f159fb09bb424dfcf61ddd83d8effc1b95fbfcd94fe' })
     },
     donate () {
-      //todo
+      this.dialogVisible = false
+      const id = this.storePID
+      const value = this.utils.toWei(this.money[id], 'ether')
+      this.toDonation(id, value)
+    },
+    openModal (id) {
+      if (!this.money[id]) {
+        return
+      }
+      this.storePID = id
+      this.dialogVisible = true
     }
+  },
+  beforeDestroy () {
+    clearInterval(this.interval)
   }
 }
 </script>
@@ -146,6 +183,16 @@ export default {
 
 .box-card
   padding 6px
+
+.number-infos
+  display flex
+  justify-content space-between
+  span
+    font-size 14px
+    display block
+  span:last-child
+    font-size 12px
+    color #bbb
 
 .clearfix:before,
 .clearfix:after
--- src/store.js
+++ src/store.js
@@ -1,17 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import moment from 'moment'
+
 import Web3 from 'web3'
 import abi from '@/donaa.abi.json'
 const w = new Web3(window.ethereum)
 const contract = new w.eth.Contract(abi)
-contract.address = '0x8e9b8701f52f30b867e6b101ccbe6a70830f9cfc'
+contract.address = '0x5479927c4cec1f3cc50a3844248d0d200592a0be'
 
 Vue.use(Vuex)
 
+const defaultData = [
+  {
+    name: '2018-19 L-Istrina Campaign',
+    intro: 'The Malta Community Chest Foundation seeks crypto donation to support its work that providing financial, material and professional support to people experiencing difficulties because of cancer or severe chronic diseases.',
+    img: 'https://resource.binance.charity/images/0257601e7b664f1cbe0b62491ee4f7b7_4.jpg',
+  },
+  {
+    name: 'Empower Bududa',
+    intro: 'Save the landslide disaster victims in Uganda.',
+    img: 'https://resource.binance.charity/images/f28bb7d8c66f42b2be995fdcc9ff819c_cancer.f120d749.jpg',
+  }
+]
+
 const store = new Vuex.Store({
   state: {
-    focusProject: ''
+    focusProject: '',
+    utils: w.utils
   },
   getters: {
   },
@@ -33,26 +49,25 @@ const store = new Vuex.Store({
           count: project.count.toNumber(),
           hash: project.hash,
           id: project.id.toNumber(),
-          starttime: project.starttime.toNumber(),
-          endtime: project.endtime.toNumber(),
+          starttime: moment(project.starttime.toNumber() * 1000).format('MMM DD YY'),
+          endtime: moment(project.endtime.toNumber() * 1000).format('MMM DD YY'),
           status: project.status,
           _type: project._type,
-          name: "2018-19 L-Istrina Campaign",
-          intro: "The Malta Community Chest Foundation seeks crypto donation to support its work that providing financial, material and professional support to people experiencing difficulties because of cancer or severe chronic diseases.",
-          img: "https://resource.binance.charity/images/0257601e7b664f1cbe0b62491ee4f7b7_4.jpg",
+          name: defaultData[i].name,
+          intro: defaultData[i].intro,
+          img: defaultData[i].img,
         }
         result.push(data)
       }
       return result
     },
-    async donation (_, { id, value, hash }) {
-      const receipt = await contract.methods.donation(id, hash).send({
+    donation (_, { id, value, hash }) {
+      contract.methods.donation(id, hash).send({
         from: window.ethereum.selectedAddress,
         value,
         gas: 200000,
         gasPrice: 1e9
       })
-      return receipt
     }
   }
 })
